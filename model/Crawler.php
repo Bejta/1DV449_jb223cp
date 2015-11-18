769c7b1a06d10bea5bc747177c4fc05d80dd45eb
Jasmin Bejtovic <jb223cp@student.lnu.se>
2015-11-18 02:34:40 +0100

    Movies are working excellent. Now only tables

--- model/Crawler.php
+++ model/Crawler.php
@@ -27,16 +27,14 @@ class Crawler{
             
             if ($dom->loadHTML($start)){
                   
-                  $links = $this->getLinks($URL.'calendar/');
+                $links = $this->getLinks($URL.'calendar/');
                   
-
-      	        //$properDays=$this->getAvailableDays($URL,$linksToSubpages[0]->getAttribute("href"));
                 $properDays=$this->getAvailableDays($links,$URL);
                 
       	        $movies=$this->getAvailableMovies($URL,$properDays);
-                //var_dump($movies);
+                
       	        $tables=$this->getTables($movies,$URL);
-                  //var_dump($tables);
+                var_dump($tables);
 
                  
                   array_push($result, array('tables'=>$tables, 'movies'=>$movies));
@@ -78,8 +76,7 @@ class Crawler{
 
                         if($time<intval(substr($comparison, 3, 2))){
                         	$tables[]=$comparison;
-                        }
-                		
+                        }     		
 
                 	}
                 	else if( $movie['day'] === "Saturday" && (strpos($comparison,'lor') !== false)){
@@ -172,42 +169,6 @@ class Crawler{
 
 	}
     
-    /*
-     *
-     * This function returns a times for movie that has avilable places on given day
-     *
-     */
-	/*public function getPossibleMovies($movieValue,$day,$movieURL){
-        
-        // Builds a query for a specific day and specific movie that comes in parameters
-		$query = "/check?day=0".$day."&movie=".$movieValue;
-
-		// Builds the whole url
-		$moviequery=$this->curl_get_request($movieURL.$query);
-
-		$moviesResult=array();
-        $dom = new DOMDocument();
-
-        if($dom->loadHTML($moviequery)){
-
-            // Decodes json object and from the array takes only those movies that has available places (status==1)
-            // Writes the start time of the available movie
-        	foreach(json_decode($moviequery) as $movie){
-        		 if($movie->status == 1){
-        		 	//array_push($moviesResult, $movie->time);
-        		 	//$moviesResult[]=array($movie->getAttribute("value"),$possibleMovies,$days[i]);
-        		 	array_push($movieResult, array('time'=>$movie['time'],'title'=>$movie->nodeValue));
-        		 }
-        	}
-        }
-        else{
-        	die("Fel vid inläsning av HTML");
-        }
-
-        return $moviesResult;
-
-	}*/
-
 	public function getAvailableMovies($URL,$days){
 
 		$moviesResult = array();
@@ -245,7 +206,7 @@ class Crawler{
         	foreach($moviedays as $day){
                   
                   if (in_array($day->nodeValue, $days)){
-                      var_dump('hej');
+                      
 
 	                  	foreach($movies as $movie){
 
@@ -262,16 +223,6 @@ class Crawler{
 	                  	}
                   }
         	}
-
-        	
-            /*
-            for($i=0;$i<$days->length;i++){
-            	foreach($movies as $movie){
-            		$possibleMovies=$this->getPossibleMovies($movie->getAttribute("value"),$days[i],$movie)
-                    $moviesResult[]=array($movie->getAttribute("value"),$possibleMovies,$days[i]);
-            	}
-            }*/
-
 
         }
         else{
